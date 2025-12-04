1c4ba6f59cd9f5ea2dd57255d5cdfc92c87c83e8
H2ODERP <110103899+H2ODERP@users.noreply.github.com>
2025-12-04 00:53:55 -0500

    Added some formatting to the text elements

--- app/src/main/java/com/example/hackchallengenewsfrontend/screens/ArticleViewScreen.kt
+++ app/src/main/java/com/example/hackchallengenewsfrontend/screens/ArticleViewScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -17,9 +19,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.hackchallengenewsfrontend.viewmodels.ArticleViewModel
@@ -39,27 +43,41 @@ private fun ArticleViewContent(
     Column(modifier = Modifier
         .fillMaxSize()
         .background(Color.Black)
-        .verticalScroll(state = rememberScrollState()),
-        horizontalAlignment = Alignment.CenterHorizontally
+        .padding(24.dp)
+        .verticalScroll(state = rememberScrollState())
     ){
-        Row(modifier = Modifier.fillMaxWidth()){
-            //TODO: Figure out proper spacing technique later
-            Spacer(modifier = Modifier.width(33.dp))
-            Text(text = "Back Btn", color = Color.White)
-            Spacer(modifier = Modifier.width(80.dp))
-            Text(text = "News Source", color = Color.White,
-                textAlign = TextAlign.Center
-            )
-            Spacer(modifier = Modifier.width(80.dp))
-            Text(text = "Bookmark Btn", color = Color.White)
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween){
+            Text(text = "Btn", color = Color.White)
+            Text(text = "News Source", color = Color.White)
+            Text(text = "Btn", color = Color.White)
         }
-        Text(text = "Title", color = Color.White)
-        Text(text = "Article Description", color = Color.White)
-        Text(text = "Date", color = Color.White)
+
+        Spacer(modifier = Modifier.height(36.dp))
+
+        Text(text = "Title", color = Color.White, fontSize = 20.sp, fontWeight = FontWeight.SemiBold)
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(text = "Article Description", color = Color.White, fontSize = 14.sp)
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(text = "Date", color = Color.LightGray, fontSize = 14.sp)
+
+        Spacer(modifier = Modifier.height(12.dp))
+
         Text(text = "INSERT IMAGE", color = Color.White)
-        Text(text = "Image Description", color = Color.White)
-        Text(text = "By Author", color = Color.White)
-        Text(text = "Article", color = Color.White)
+
+        Text(text = "Image Description", color = Color.LightGray, fontSize = 12.sp)
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(text = "By Author", color = Color.White, fontSize = 15.sp)
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(text = "Article", color = Color.White, fontSize = 15.sp)
     }
 }
 
@@ -69,5 +87,52 @@ fun ArticleViewScreenPreview(){
     ArticleViewContent(
         uiState = ArticleViewModel.ArticleUIState()
     )
+}
 
+@Preview(showBackground = true)
+@Composable
+fun TestingArticleView(){
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Black)
+        .padding(24.dp)
+        .verticalScroll(state = rememberScrollState())
+    ){
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween){
+            Text(text = "Btn", color = Color.White)
+            Text(text = "News Source", color = Color.White)
+            Text(text = "Btn", color = Color.White)
+        }
+
+        Spacer(modifier = Modifier.height(36.dp))
+
+        Text(text = "We do a little trolling! This is a long string of text for the title hahahahaha hahhahaha", color = Color.White, fontSize = 20.sp, fontWeight = FontWeight.SemiBold)
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(text = "Article description Article description Article description Article description Article description Article description Article description Article description Article description ", color = Color.White, fontSize = 14.sp)
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(text = "Date", color = Color.LightGray, fontSize = 14.sp)
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(text = "INSERT IMAGE", color = Color.White)
+
+        Text(text = "Image Description Image Description Image Description Image Description Image Description Image Description Image Description", color = Color.LightGray, fontSize = 12.sp)
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(text = "By Author", color = Color.White, fontSize = 15.sp)
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(text = "man & girl go out to drive under moonlight. they stop at on at a side of road. he turn to his girl and say: \"baby, i love you very much\" \"what is it honey?\" \"our car is broken down. i think the engine is broken, ill walk and get some more fuel.\" \"ok. ill stay here and look after our stereo. there have been news report of steres being stolen.\" \"good idea. keep the doors locked no matter what. i love you sweaty\"\n" +
+                "\n" +
+                "so the guy left to get full for the car. after two hours the girl say \"where is my baby, he was supposed to be back by now\". then the girl here a scratching sound and a voice say \"LET ME IN\"\n" +
+                "\n" +
+                "the girl doesn't do it and then after a while she goes to sleep. the next morning she wakes up and finds her boyfriend still not there. she gets out to check and man door hand hook car door.", color = Color.White, fontSize = 15.sp)
+    }
 }
