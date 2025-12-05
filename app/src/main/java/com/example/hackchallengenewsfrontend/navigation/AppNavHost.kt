3a6ad61320cea2a15a1b882d4461b37d0d2fcb06
H2ODERP <110103899+H2ODERP@users.noreply.github.com>
2025-12-04 20:57:51 -0500

    added UI to MainListenScreen

---
+++ app/src/main/java/com/example/hackchallengenewsfrontend/navigation/AppNavHost.kt
@@ -0,0 +1,60 @@
+package com.example.hackchallengenewsfrontend.navigation
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.hackchallengenewsfrontend.screens.NewsScreen
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ScopeApp (){
+    val navController = rememberNavController()
+    Scaffold(modifier = Modifier.fillMaxSize(),
+        bottomBar = { BottomNavigationBar(navController = navController) }) { innerPadding ->
+        Box(modifier = Modifier.padding(top = innerPadding.calculateTopPadding())) {
+            SetupNavHost(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationBar(navController : NavHostController){
+
+}
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun SetupNavHost(navController : NavHostController){
+    NavHost(
+        navController = navController,
+        startDestination = "NewsScreen"
+    ) {
+        composable("NewsScreen") {
+            NewsScreen(viewArticle = { url ->
+                val encodedUrl = URLEncoder.encode(url, StandardCharsets.UTF_8.toString())
+                navController.navigate("article/$encodedUrl")
+            })
+        }
+        composable(
+            "article/{url}",
+            arguments = listOf(navArgument("url") {type = NavType.StringType})
+        ) { backStackEntry ->
+            Text("This is the article screen for: ${backStackEntry.arguments?.getString("url")}")
+        }
+    }
+}
--- app/src/main/java/com/example/hackchallengenewsfrontend/screens/MainListenScreen.kt
+++ app/src/main/java/com/example/hackchallengenewsfrontend/screens/MainListenScreen.kt
@@ -1,8 +1,117 @@
 package com.example.hackchallengenewsfrontend.screens
 
+import android.R.attr.author
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.hackchallengenewsfrontend.components.CompactNewsCard
+import com.example.hackchallengenewsfrontend.components.FilterRow
+import com.example.hackchallengenewsfrontend.components.NewsCard
+import com.example.hackchallengenewsfrontend.viewmodels.NewsViewModel
 
 @Composable
-fun MainListenScreen(){
-    //Home Screen Clone kinda
+fun MainListenScreen(
+    viewArticle: (String) -> Unit
+) {
+    LazyColumn(
+        Modifier
+            .fillMaxSize()
+            .background(color = Color.Black)
+            .padding(top = 30.dp, start = 22.dp, end = 22.dp)
+    ) {
+        // Header
+        item {
+            Text(
+                "Scope",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 2.dp),
+                fontSize = 40.sp,
+                textAlign = TextAlign.Left,
+                color = Color.White
+            )
+            Text(
+                "Home",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 2.dp),
+                fontSize = 20.sp,
+                textAlign = TextAlign.Left,
+                color = Color.LightGray
+            )
+            Spacer(Modifier.height(24.dp))
+            Text(
+                "Editor's Picks",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 2.dp),
+                fontSize = 20.sp,
+                textAlign = TextAlign.Left,
+                color = Color.White
+            )
+            Spacer(Modifier.height(12.dp))
+        }
+
+        // Body
+        //TODO Change NewsCard/CompactNewsCard to be not hard-coded
+        item {
+            NewsCard(
+                newsSource = "Cornell Chronicle",
+                title = "Winter storm snarls flights for post-Thanksgiving travelers in Chicago",
+                thumbnailUrl = "https://d3i6fh83elv35t.cloudfront.net/static/2025/11/GettyImages-2248617554-1200x800.jpg",
+                thumbnailDescription = "Winter Storm Snarls Air Travel In Chicago",
+                onCardClick = { viewArticle("https://www.pbs.org/newshour/nation/winter-storm-snarls-flights-for-post-thanksgiving-travelers-in-chicago") },
+                author = "Author",
+                date = "Date"
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+
+        item {
+            Text(
+                "Audio Stories For You",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 2.dp),
+                fontSize = 20.sp,
+                textAlign = TextAlign.Left,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+        }
+
+        item {
+            CompactNewsCard(
+                title = "TEMP TITLE",
+                newsSource = "TEMP SOURCE",
+                author = "TEMP AUTHOR",
+                thumbnailUrl = "TEMP URL",
+                thumbnailDescription = "TEMP DESC",
+                onCardClick = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainListenScreenPreview(){
+    MainListenScreen(viewArticle = {})
 }
